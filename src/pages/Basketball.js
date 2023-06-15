0e3b029f5b4e6c9dbd2edf008f6fe7c94e18e676
traccoenlea <leatraccoen@gmail.com>
2023-06-15 15:32:58 +0200

    tp fini

--- src/pages/Basketball.js
+++ src/pages/Basketball.js
@@ -1,84 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Basketball() {
   const teams = [
-    { id: 0, name: "Spurs" },
-    { id: 1, name: "Heat" },
-    { id: 2, name: "Lakers" },
-    { id: 3, name: "Cavaliers" },
-    // { id: 4, name: "Bucks" },
-    // { id: 5, name: "Nets" },
-    // { id: 6, name: "76ers" },
-    // { id: 7, name: "Celtics" },
-    // { id: 8, name: "Magic" },
-    // { id: 9, name: "Hornets" },
-    // { id: 10, name: "Suns" },
-    // { id: 11, name: "Clippers" },
-    // { id: 12, name: "Wizards" },
-    // { id: 13, name: "Warriors" },
-    // { id: 14, name: "Nuggets" },
-    // { id: 15, name: "Thunder" },
-    // { id: 16, name: "Kings" },
-    // { id: 17, name: "Pelicans" },
-    // { id: 18, name: "Timberwolves" },
-    // { id: 19, name: "Jazz" },
+    "Spurs",
+    "Heat",
+    "Lakers",
+    "Cavaliers",
+    "Bucks",
+    "Nets",
+    "76ers",
+    "Celtics",
+    "Magic",
+    "Benoit",
+    "Hornets",
+    "Suns",
+    "Clippers",
+    "Wizards",
+    "Warriors",
+    "Nuggets",
+    "Thunder",
+    "Kings",
+    "Pelicans",
+    "Timberwolves",
+    "Jazz",
   ];
 
-  const [teamList, setTeamList] = useState([]);
-  const [teamDel, setTeamDel] = useState(teams);
-  const [size, setSize] = useState(0);
+  const [teamsList, setTeamList] = useState([]);
+  const usedIndexes = [];
 
-  const removeItem = (number) => {
-    setTeamDel((current) => current.filter((teamDel) => teamDel.id !== number));
-  };
+  useEffect(() => {
+    async function getRandomArray() {
+      const interval = setInterval(() => {
+        if (usedIndexes.length === teams.length) {
+          clearInterval(interval);
+          return;
+        }
 
-  async function test() {
-    const size = teamDel.length;
-    console.log(size);
-    // for (let i = 0; i < size; i++) {
-    //   console.log(i);
-    // }
-    const teamListLenght = teamList.length;
-    let n = teamListLenght;
-    console.log(n);
-    while (n < size) {
-      console.log(n);
-      n++;
+        let randomIndex;
+        do {
+          randomIndex = Math.floor(Math.random() * teams.length);
+        } while (usedIndexes.includes(randomIndex));
+
+        usedIndexes.push(randomIndex);
+        const randomName = teams[randomIndex];
+        console.log(randomName);
+        // teamsList.push(randomName);
+        setTeamList((teamsList) => [...teamsList, randomName]);
+        console.log(teamsList);
+      });
     }
-    const number = Math.floor(Math.random() * size);
-    setTeamList((teamList) => [...teamList, number]);
-    removeItem(number);
-    console.log("size");
-    console.log(teamDel.length);
-  }
+    getRandomArray();
+  }, []);
 
-  function getRandNumber(size) {
-    return Math.floor(Math.random() * size);
-  }
-
-  function launchRandom() {
-    let n = 0;
-  }
-
-  console.log(size);
   return (
-    <div className="flex flexc mauto mt30">
-      <h1>Basketball</h1>
-      <p>
-        Réaliser un random de 20 éléments d’un tableau. Chaque élément doit
-        sortir au moins une fois lors des 20 premiers tours mais jamais deux
-        fois.
-      </p>
-      <button className="btn" onClick={launchRandom}>
-        Random
-      </button>
-      <div className="mauto mt30">
-        <ol>
-          {teams.map((team, i) => (
-            <li key={i}>{team.name}</li>
-          ))}
-        </ol>
-      </div>
+    <div className="mauto mt50">
+      <h1>Les équipes de basket</h1>
+      <ol className="mt30">
+        {teamsList.map((team, i) => (
+          <li key={i}>{team}</li>
+        ))}
+      </ol>
     </div>
   );
 }
